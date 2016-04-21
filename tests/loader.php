fc8267a337c12cb340e4eb49a863431fbe495e80
Mike Thorn <mike@therapycharts.com>
2016-04-21 11:55:13 -0400

    changing loader to bootstrap, using autoload now, fixing up composer autoload-dev entry

--- tests/loader.php
+++
@@ -1,5 +0,0 @@
-<?php
-include(__DIR__.'/../src/php/ContainerInterface.php');
-include(__DIR__.'/../src/php/Container.php');
-include(__DIR__.'/../src/php/RequestContainer.php');
-include(__DIR__.'/../src/php/CookieContainer.php');
