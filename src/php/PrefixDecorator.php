01a7bf72b0046fe05c925ac42d961ef3c455b246
Mike Thorn <mike@therapycharts.com>
2016-04-22 09:43:20 -0400

    adding new prefix decorator class

---
+++ src/php/PrefixDecorator.php
@@ -0,0 +1,60 @@
+<?php
+namespace Lucid\Component\Container;
+
+class PrefixDecorator
+{
+    protected $prefix    = null;
+    protected $container = null;
+    
+    function __construct(string $prefix = '', $container)
+    {
+        $this->prefix = $prefix;
+        $this->container = $container;
+    }
+
+    public function has(string $id)
+    {
+        return $this->container->has($this->prefix.$id);
+    }
+
+    public function &get(string $id, $defaultValue = null)
+    {
+        $value = &$this->container->get($this->prefix.$id, $defaultValue = null);
+        return $value;
+    }
+
+    public function un_set(string $id)
+    {
+        return $this->container->un_set($this->prefix.$id);
+    }
+
+    public function string(string $id, $defaultValue = null)
+    {
+        return $this->container->string($this->prefix.$id, $defaultValue);
+    }
+
+    public function int(string $id, $defaultValue = null)
+    {
+        return $this->container->int($this->prefix.$id, $defaultValue);
+    }
+
+    public function float(string $id, $defaultValue = null)
+    {
+        return $this->container->float($this->prefix.$id, $defaultValue);
+    }
+
+    public function bool(string $id, $defaultValue = null)
+    {
+        return $this->container->bool($this->prefix.$id, $defaultValue);
+    }
+
+    public function DateTime(string $id, $defaultValue = null)
+    {
+        return $this->container->DateTime($this->prefix.$id, $defaultValue);
+    }
+
+    public function __call($method, $parameters)
+    {
+        return $this->container->$method(...$parameters);
+    }
+}
